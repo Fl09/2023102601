7103d4f0bef76f89fc48b599e384c17d25683bce
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-10-26 13:59:08 -0400

    Create Exercício 2023102601.h

---
+++ Exercício 2023102601.h
@@ -0,0 +1,22 @@
+//Criar um tipo abstrato de dados que represente uma pessoa, contendo: nome, data de nascimento e CPF.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+  char nome, cpf, data;
+
+  printf("Digite seu nome: ");
+  scanf("%s", &nome);
+
+  printf("Digite seu CPF: ");
+  scanf("%s", &cpf);
+  
+  printf("Digite sua data de nascimento: ");
+  scanf("%s", &data);
+
+  
+  
+  return 0;
+}
