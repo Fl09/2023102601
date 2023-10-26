34d3c626bcdae6864c87e79b06f6b9a350387da7
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-10-26 12:08:13 -0400

    Update Ponto.h

--- Ponto.h
+++ Ponto.h
@@ -1,1 +1,16 @@
-d
+#ifndef PONTO_H_INCLUDED
+#define PONTO_H_INCLUSED
+
+struct ponto{
+   float x;
+   float y;
+};
+
+trypedef struct ponto Ponto;
+
+Ponto* Cria_ponto (float x, float y);
+void Libera_ponto (Ponto * pto);
+int Acessa_ponto (Ponto * pto, float *x, float * y);
+float Distancia_ponto (Ponto *pto1, Ponto *pto2);
+
+#endif
