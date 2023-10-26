2d7b29edcc2a124ef9a87f41c6c54f0bcc47c054
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-10-26 13:45:13 -0400

    Update Ponto.h

--- Ponto.h
+++ Ponto.h
@@ -1,11 +1,15 @@
-#ifndef PONTO_H_INCLUDED
-#define PONTO_H_INCLUSED
+#include <stdio.h>
+#include <stdlib.h>
 
-struct ponto{
+#ifndef PONTO_H_INCLUDED
+#define PONTO_H_INCLUDED
+
+int main (){
    float x;
    float y;
-};
-
+   int* pto1;
+   int* pto2;
+   
 typedef struct ponto Ponto;
 
 Ponto* Cria_ponto (float x, float y);
@@ -13,4 +17,12 @@ void Libera_ponto (Ponto * pto);
 int Acessa_ponto (Ponto * pto, float *x, float * y);
 float Distancia_ponto (Ponto *pto1, Ponto *pto2);
 
+printf("Digite o valor de x: ");
+scanf("%f", &x);
+printf("Digite o valor de y: ");
+scanf("%f", &y);
+
+  
+return 0;
 #endif
+}
