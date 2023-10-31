fc575994c403f4784ee6682a5dc455c90a3edc7d
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-10-30 21:13:14 -0400

    Create 2023102604 complexo.c

---
+++ 2023102604 complexo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include "complexo.h"
+
+complexo complexo_cria(double x, double y){
+  complexo c;
+  c.x = x;
+  c.y= y;
+  return c;
+}
+
+complexo complexo_soma(complexo a, complexo b){
+  return complexo_cria(a.x + b.x, a.y + b.y);
+}
+
+complexo complexo_subtracao(complexo a, complexo b){
+  return complexo_cria(a.x - b.x, a.y - b.y);
+}
+
+complexo complexo_multiplicacao(complexo a, complexo b){
+  return complexo_cria(a.x * b.x, a.y * b.y);
+}
+
+complexo complexo_divisao(complexo a, complexo b){
+  return complexo_cria(a.x / b.x, a.y / b.y);
+}
