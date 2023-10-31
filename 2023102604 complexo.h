444ef09f0779e7f9e26a3f8f1390abecf99c65db
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-10-30 21:00:03 -0400

    2023102604 complexo.h

---
+++ 2023102604 complexo.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <srdlib.h>
+#include <math.h>
+
+typedef struct{
+  double x;
+  double y;
+} complexo;
+
+complexo complexo_crie (double x, doubley);
+complexo complexo_destrua ();
+complexo complexo_soma (complexo a, complexo b);
+complexo complexo_subtracao (complexo a, complexo b);
+complexo complexo_multiplicacao(complexo a, complexo b);
+complexo complexo_divisao (complexo a, complexo b);
