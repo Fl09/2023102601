44f9b366304f264111d59efb256adb705ae641ab
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-11-13 17:28:15 -0400

    turma.h

---
+++ turma.h
@@ -0,0 +1,17 @@
+#ifndef TURMA_H
+#define TURMA_H
+#include "aluno.h"
+
+typedef struct {
+
+  Aluno* alunos;
+  int quantidade;
+} Turma;
+
+Turma criarTurma (int quantidade);
+void inserirAluno (Turma* turma, Aluno aluno, int posicao);
+void liberarTurma (Turma* turma);
+void buscarAluno (Turma turma, int codigo);
+void imprimirTurma (Turma turma);
+
+#endif
