a654662034b15412c0f9ba7d6d2f181250b3fcbd
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-11-13 22:48:44 -0400

    Delete functions.h
    delete

--- functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-int findMax(int arr[], int size);
-int findMin(int arr[], int size);
-int calculateSum (int arr[], int size);
-int calculateProduct(int arr[], int size);
-
-#endif
