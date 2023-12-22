9c0e6589b031b8146fa58d53ef5dc2ffa3205c65
Flavia2023 <111545886+Fl09@users.noreply.github.com>
2023-12-22 15:00:56 -0400

    Create 20231219 - Pilhazz.c

---
+++ 20231219 - Pilhazz.c
@@ -0,0 +1,7 @@
+void inicializarHangar(Pilha* hangar);
+void adicionarAviao(Pilha* hangar, Aviao aviao);
+void listarAvioes(Pilha hangar);
+void listarPrimeiroAviao(Pilha hangar);
+void tirarAviao(Pilha* hangar);
+void tirarAviaoPorPosicao(Pilha* hangar, int posicao);
+int contarAvioes(Pilha hangar);
